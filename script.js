dc9b177a5cc232ac739b78dacdfa71fccb50ef31
Rahul <thakur.rahul@rwit.io>
2025-07-10 14:07:39 +0530

    Update index.html, script.js, and add style.css for GSAP animation implementation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hello bhai")
+
+
+
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.to('#box2',{
+        x :'-568%',
+        duration:3
+    })
+   });
